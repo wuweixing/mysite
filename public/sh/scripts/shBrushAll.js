96b29b9a35d28a1ceb84c0e7a72485368d059b17
Ruchee <my@ruchee.com>
2013-08-22 18:21:21 +0800

    daily backup at company

--- public/sh/scripts/shBrushAll.js
+++ public/sh/scripts/shBrushAll.js
@@ -68,89 +68,7 @@ SyntaxHighlighter.brushes.Cpp.prototype = new SyntaxHighlighter.Highlighter();
 SyntaxHighlighter.brushes.Cpp.aliases   = ['c', 'cpp', 'C', 'C++'];
 
 
-// For D
-(function() {
-    typeof(require) != 'undefined' ? SyntaxHighlighter = require('shCore').SyntaxHighlighter : null;
-
-    function Brush() {
-        var builtins  = 'toString toHash opCmp opEquals ' +
-                        'opUnary opBinary opApply opCall opAssign opIndexAssign opSliceAssign opOpAssign ' +
-                        'opIndex opSlice opDispatch ' +
-                        'toString toHash opCmp opEquals Monitor factory classinfo vtbl offset getHash ' +
-                        'equals compare tsize swap next init flags offTi destroy postblit toString toHash ' +
-                        'factory classinfo Throwable Exception Error capacity reserve assumeSafeAppend clear ' +
-                        'ModuleInfo ClassInfo MemberInfo TypeInfo';
-
-        var properties = '.sizeof .stringof .mangleof .nan .init .alignof .max .min .infinity .epsilon .mant_dig ' +
-                        '.max_10_exp .max_exp .min_10_exp .min_exp .min_normal .re .im';
-
-        var special_tokens = '__FILE__ __LINE__ __DATE__ __EOF__ __TIME__ __TIMESTAMP__ __VENDOR__ __VERSION__ #line';
-
-        var keywords =  '@property @disable abstract alias align asm assert auto body bool break byte case cast catch ' +
-                        'cdouble cent cfloat char class const continue creal dchar debug default delegate delete deprecated ' +
-                        'do double else enum export extern false final finally float for foreach foreach_reverse ' +
-                        'function goto idouble if ifloat immutable import in inout int interface invariant ireal ' +
-                        'is lazy long macro mixin module new nothrow null out override package pragma private ' +
-                        'protected public pure real ref return scope shared short static struct super switch ' +
-                        'synchronized template this throw true try typedef typeid typeof ubyte ucent uint ulong ' +
-                        'union unittest ushort version void volatile wchar while with __gshared ' +
-                        '__thread __traits ' +
-                        'string wstring dstring size_t hash_t ptrdiff_t equals_t ';
-
-        this.regexList = [
-            { regex: /\/\/\/.*$/gm, css: 'color3' },
-            { regex: SyntaxHighlighter.regexLib.singleLineCComments, css: 'comments' },
-            { regex: SyntaxHighlighter.regexLib.multiLineCComments, css: 'comments' },
-            { regex: /\/\+[\s\S]*?\+\//g, css: 'color1' },
-            { regex: SyntaxHighlighter.regexLib.multiLineDoubleQuotedString, css: 'string' },
-            { regex: SyntaxHighlighter.regexLib.multiLineSingleQuotedString, css: 'string' },
-            { regex: SyntaxHighlighter.regexLib.doubleQuotedString, css: 'string' },
-            { regex: SyntaxHighlighter.regexLib.singleQuotedString, css: 'string' },
-            { regex: new RegExp(this.getKeywords(properties), 'gm'), css: 'color2' },
-            { regex: new RegExp(this.getKeywords(special_tokens), 'gm'), css: 'constants' },
-            { regex: new RegExp(this.getKeywords(builtins), 'gm'), css: 'color2' },
-            { regex: new RegExp(this.getKeywords(keywords), 'gm'), css: 'keyword' }
-        ];
-    };
-
-    Brush.prototype = new SyntaxHighlighter.Highlighter();
-    Brush.aliases   = ['d', 'D'];
-
-    SyntaxHighlighter.brushes.D = Brush;
-    typeof(exports) != 'undefined' ? exports.Brush = Brush : null;
-})();
-
-
-// For Erlang
-(function() {
-    typeof(require) != 'undefined' ? SyntaxHighlighter = require('shCore').SyntaxHighlighter : null;
-
-    function Brush() {
-        var keywords = 'after and andalso band begin bnot bor bsl bsr bxor '+
-            'case catch cond div end fun if let not of or orelse '+
-            'query receive rem try when xor'+
-            ' module export import define';
-
-        this.regexList = [
-            { regex: new RegExp("[A-Z][A-Za-z0-9_]+", 'g'), css: 'constants' },
-            { regex: new RegExp("\\%.+", 'gm'), css: 'comments' },
-            { regex: new RegExp("\\?[A-Za-z0-9_]+", 'g'), css: 'preprocessor' },
-            { regex: new RegExp("[a-z0-9_]+:[a-z0-9_]+", 'g'), css: 'functions' },
-            { regex: SyntaxHighlighter.regexLib.doubleQuotedString, css: 'string' },
-            { regex: SyntaxHighlighter.regexLib.singleQuotedString, css: 'string' },
-            { regex: new RegExp(this.getKeywords(keywords), 'gm'), css: 'keyword' }
-        ];
-    };
-
-    Brush.prototype = new SyntaxHighlighter.Highlighter();
-    Brush.aliases   = ['erlang', 'Erlang'];
-
-    SyntaxHighlighter.brushes.Erland = Brush;
-    typeof(exports) != 'undefined' ? exports.Brush = Brush : null;
-})();
-
-
-// For Scheme/Racket/CommonLisp
+// For Lisp
 SyntaxHighlighter.brushes.Lisp = function(){
     var funcs     = 'lambda list progn mapcar car cdr reverse member append format';
     var keywords  = 'let while unless cond if eq t nil defvar dotimes setf listp numberp not equal';
@@ -171,166 +89,6 @@ SyntaxHighlighter.brushes.Lisp = function(){
 
 SyntaxHighlighter.brushes.Lisp.prototype = new SyntaxHighlighter.Highlighter();
 SyntaxHighlighter.brushes.Lisp.aliases   = ['lisp', 'Lisp'];
-
-
-// For Clojure
-function ClojureRegExp(pattern) {
-    pattern = pattern + '(?=[[\\]{}(),\\s])';
-    this.regex = new RegExp(pattern, 'g');
-    this.lookBehind = /[\[\]{}(),\s]$/;
-}
-
-ClojureRegExp.prototype.exec = function (str) {
-    var match, leftContext;
-    while (match=this.regex.exec(str)) {
-        leftContext = str.substring(0, match.index);
-        if (this.lookBehind.test(leftContext)) {
-            return match;
-        }
-        else {
-            this.regex.lastIndex = match.index + 1;
-        }
-    }
-    return null;
-};
-
-SyntaxHighlighter.brushes.Clojure = function () {
-    var special_forms =
-        '. def do fn if let loop monitor-enter monitor-exit new quote recur set! ' +
-        'throw try var',
-        clojure_core =
-            '* *1 *2 *3 *agent* *allow-unresolved-vars* *assert* *clojure-version* ' +
-            '*command-line-args* *compile-files* *compile-path* *e *err* *file* ' +
-            '*flush-on-newline* *in* *macro-meta* *math-context* *ns* *out* ' +
-            '*print-dup* *print-length* *print-level* *print-meta* *print-readably* ' +
-            '*read-eval* *source-path* *use-context-classloader* ' +
-            '*warn-on-reflection* + - -> -&gt; ->> -&gt;&gt; .. / < &lt; <= &lt;= = ' +
-            '== > &gt; >= &gt;= accessor aclone ' +
-            'add-classpath add-watch agent agent-errors aget alength alias all-ns ' +
-            'alter alter-meta! alter-var-root amap ancestors and apply areduce ' +
-            'array-map aset aset-boolean aset-byte aset-char aset-double aset-float ' +
-            'aset-int aset-long aset-short assert assoc assoc! assoc-in associative? ' +
-            'atom await await-for await1 bases bean bigdec bigint binding bit-and ' +
-            'bit-and-not bit-clear bit-flip bit-not bit-or bit-set bit-shift-left ' +
-            'bit-shift-right bit-test bit-xor boolean boolean-array booleans ' +
-            'bound-fn bound-fn* butlast byte byte-array bytes cast char char-array ' +
-            'char-escape-string char-name-string char? chars chunk chunk-append ' +
-            'chunk-buffer chunk-cons chunk-first chunk-next chunk-rest chunked-seq? ' +
-            'class class? clear-agent-errors clojure-version coll? comment commute ' +
-            'comp comparator compare compare-and-set! compile complement concat cond ' +
-            'condp conj conj! cons constantly construct-proxy contains? count ' +
-            'counted? create-ns create-struct cycle dec decimal? declare definline ' +
-            'defmacro defmethod defmulti defn defn- defonce defstruct delay delay? ' +
-            'deliver deref derive descendants destructure disj disj! dissoc dissoc! ' +
-            'distinct distinct? doall doc dorun doseq dosync dotimes doto double ' +
-            'double-array doubles drop drop-last drop-while empty empty? ensure ' +
-            'enumeration-seq eval even? every? false? ffirst file-seq filter find ' +
-            'find-doc find-ns find-var first float float-array float? floats flush ' +
-            'fn fn? fnext for force format future future-call future-cancel ' +
-            'future-cancelled? future-done? future? gen-class gen-interface gensym ' +
-            'get get-in get-method get-proxy-class get-thread-bindings get-validator ' +
-            'hash hash-map hash-set identical? identity if-let if-not ifn? import ' +
-            'in-ns inc init-proxy instance? int int-array integer? interleave intern ' +
-            'interpose into into-array ints io! isa? iterate iterator-seq juxt key ' +
-            'keys keyword keyword? last lazy-cat lazy-seq let letfn line-seq list ' +
-            'list* list? load load-file load-reader load-string loaded-libs locking ' +
-            'long long-array longs loop macroexpand macroexpand-1 make-array ' +
-            'make-hierarchy map map? mapcat max max-key memfn memoize merge ' +
-            'merge-with meta method-sig methods min min-key mod name namespace neg? ' +
-            'newline next nfirst nil? nnext not not-any? not-empty not-every? not= ' +
-            '  ns ns-aliases ns-imports ns-interns ns-map ns-name ns-publics ' +
-            'ns-refers ns-resolve ns-unalias ns-unmap nth nthnext num number? odd? ' +
-            'or parents partial partition pcalls peek persistent! pmap pop pop! ' +
-            'pop-thread-bindings pos? pr pr-str prefer-method prefers ' +
-            'primitives-classnames print print-ctor print-doc print-dup print-method ' +
-            'print-namespace-doc print-simple print-special-doc print-str printf ' +
-            'println println-str prn prn-str promise proxy proxy-call-with-super ' +
-            'proxy-mappings proxy-name proxy-super push-thread-bindings pvalues quot ' +
-            'rand rand-int range ratio? rational? rationalize re-find re-groups ' +
-            're-matcher re-matches re-pattern re-seq read read-line read-string ' +
-            'reduce ref ref-history-count ref-max-history ref-min-history ref-set ' +
-            'refer refer-clojure release-pending-sends rem remove remove-method ' +
-            'remove-ns remove-watch repeat repeatedly replace replicate require ' +
-            'reset! reset-meta! resolve rest resultset-seq reverse reversible? rseq ' +
-            'rsubseq second select-keys send send-off seq seq? seque sequence ' +
-            'sequential? set set-validator! set? short short-array shorts ' +
-            'shutdown-agents slurp some sort sort-by sorted-map sorted-map-by ' +
-            'sorted-set sorted-set-by sorted? special-form-anchor special-symbol? ' +
-            'split-at split-with str stream? string? struct struct-map subs subseq ' +
-            'subvec supers swap! symbol symbol? sync syntax-symbol-anchor take ' +
-            'take-last take-nth take-while test the-ns time to-array to-array-2d ' +
-            'trampoline transient tree-seq true? type unchecked-add unchecked-dec ' +
-            'unchecked-divide unchecked-inc unchecked-multiply unchecked-negate ' +
-            'unchecked-remainder unchecked-subtract underive unquote ' +
-            'unquote-splicing update-in update-proxy use val vals var-get var-set ' +
-            'var? vary-meta vec vector vector? when when-first when-let when-not ' +
-            'while with-bindings with-bindings* with-in-str with-loading-context ' +
-            'with-local-vars with-meta with-open with-out-str with-precision xml-seq ' +
-            'zero? zipmap ';
-
-    this.getKeywords = function (keywordStr) {
-        keywordStr = keywordStr.replace(/[\-\[\]{}()*+?.\\\^$|,#]/g, "\\$&");
-        keywordStr = keywordStr.replace(/^\s+|\s+$/g, '').replace(/\s+/g, '|');
-        return '(?:' + keywordStr + ')';
-    };
-
-    this.regexList = [
-        { regex: new RegExp(';.*$', 'gm'), css: 'comments' },
-        { regex: SyntaxHighlighter.regexLib.multiLineDoubleQuotedString, css: 'string' },
-        { regex: /#"(?:\.|(\\\")|[^\""\n])*"/g, css: 'string' },
-        { regex: /\[|\]/g, css: 'keyword' },
-        { regex: /&(amp;)?/g, css: 'keyword' },
-        { regex: /#?\{|\}/g, css: 'keyword' },
-        { regex: /#\^\{/g, css: 'keyword' },
-        { regex: /#\(|%/g, css: 'keyword' },
-        { regex: /@/g, css: 'keyword' },
-        { regex: /(['`]|~@?)[\[({]/g, css: 'keyword' },
-        { regex: /\(|\)/g, css: 'keyword' },
-        { regex: /\\.\b/g, css: 'value' },
-        { regex: /[+\-]?\b0x[0-9A-F]+\b/gi, css: 'value' },
-        { regex: new ClojureRegExp("[+-]?\\b\\d+(\\.\\d*)?([eE][+-]?\\d+|M)?\\b"), css: 'value' },
-        { regex: /^[+\-]?\b\d+(\.\d*)?([eE][+\-]?\d+|M)?\b/g, css: 'value' },
-        { regex: /\b(true|false|nil)\b/g, css: 'value' },
-        { regex: /(`|#?'|~@?)[\w-.\/]+/g, css: 'color1' },
-        { regex: /:[A-Za-z0-9_\-]+/g, css: 'constants' },
-        { regex: new ClojureRegExp(this.getKeywords(special_forms)), css: 'preprocessor' },
-        { regex: /\#\^[A-Za-z]\w*/g, css: 'preprocessor' },
-        { regex: new ClojureRegExp(this.getKeywords(clojure_core)), css: 'functions' }
-    ];
-
-    this.forHtmlScript(SyntaxHighlighter.regexLib.scriptScriptTags);
-};
-
-SyntaxHighlighter.brushes.Clojure.prototype = new SyntaxHighlighter.Highlighter();
-SyntaxHighlighter.brushes.Clojure.aliases   = ['clojure', 'Clojure'];
-
-
-// For Ruby
-SyntaxHighlighter.brushes.Ruby = function() {
-    var keywords = 'alias and BEGIN begin break case class def define_method defined do each else elsif ' +
-        'END end ensure false for if in module new next nil not or raise redo rescue retry return ' +
-        'self super then throw true undef unless until when while yield';
-
-    var builtins = 'Array Bignum Binding Class Continuation Dir Exception FalseClass File::Stat File Fixnum Fload ' +
-        'Hash Integer IO MatchData Method Module NilClass Numeric Object Proc Range Regexp String Struct::TMS Symbol ' +
-        'ThreadGroup Thread Time TrueClass';
-
-    this.regexList = [
-        { regex: SyntaxHighlighter.regexLib.singleLinePerlComments, css: 'comments' },
-        { regex: SyntaxHighlighter.regexLib.doubleQuotedString, css: 'string' },
-        { regex: SyntaxHighlighter.regexLib.singleQuotedString, css: 'string' },
-        { regex: /\b[A-Z0-9_]+\b/g, css: 'constants' },
-        { regex: /:[a-z][A-Za-z0-9_]*/g, css: 'color2' },
-        { regex: /(\$|@@|@)\w+/g, css: 'variable bold' },
-        { regex: new RegExp(this.getKeywords(keywords), 'gm'), css: 'keyword' },
-        { regex: new RegExp(this.getKeywords(builtins), 'gm'),  css: 'color1' }
-    ];
-
-    this.forHtmlScript(SyntaxHighlighter.regexLib.aspScriptTags);
-};
-
-SyntaxHighlighter.brushes.Ruby.prototype = new SyntaxHighlighter.Highlighter();
-SyntaxHighlighter.brushes.Ruby.aliases  = ['ruby', 'Ruby'];
 
 
 // For PHP
@@ -395,6 +153,34 @@ SyntaxHighlighter.brushes.Php = function() {
 
 SyntaxHighlighter.brushes.Php.prototype = new SyntaxHighlighter.Highlighter();
 SyntaxHighlighter.brushes.Php.aliases = ['php', 'PHP'];
+
+
+// For Ruby
+SyntaxHighlighter.brushes.Ruby = function() {
+    var keywords = 'alias and BEGIN begin break case class def define_method defined do each else elsif ' +
+        'END end ensure false for if in module new next nil not or raise redo rescue retry return ' +
+        'self super then throw true undef unless until when while yield';
+
+    var builtins = 'Array Bignum Binding Class Continuation Dir Exception FalseClass File::Stat File Fixnum Fload ' +
+        'Hash Integer IO MatchData Method Module NilClass Numeric Object Proc Range Regexp String Struct::TMS Symbol ' +
+        'ThreadGroup Thread Time TrueClass';
+
+    this.regexList = [
+        { regex: SyntaxHighlighter.regexLib.singleLinePerlComments, css: 'comments' },
+        { regex: SyntaxHighlighter.regexLib.doubleQuotedString, css: 'string' },
+        { regex: SyntaxHighlighter.regexLib.singleQuotedString, css: 'string' },
+        { regex: /\b[A-Z0-9_]+\b/g, css: 'constants' },
+        { regex: /:[a-z][A-Za-z0-9_]*/g, css: 'color2' },
+        { regex: /(\$|@@|@)\w+/g, css: 'variable bold' },
+        { regex: new RegExp(this.getKeywords(keywords), 'gm'), css: 'keyword' },
+        { regex: new RegExp(this.getKeywords(builtins), 'gm'),  css: 'color1' }
+    ];
+
+    this.forHtmlScript(SyntaxHighlighter.regexLib.aspScriptTags);
+};
+
+SyntaxHighlighter.brushes.Ruby.prototype = new SyntaxHighlighter.Highlighter();
+SyntaxHighlighter.brushes.Ruby.aliases  = ['ruby', 'Ruby'];
 
 
 // For CoffeeScript
@@ -722,7 +508,7 @@ SyntaxHighlighter.brushes.Vim.prototype  = new SyntaxHighlighter.Highlighter();
 SyntaxHighlighter.brushes.Vim.aliases    = ['vim', 'Vim'];
 
 
-// For Sass
+// For Sass/Scss
 (function () {
     typeof(require) != 'undefined' ? SyntaxHighlighter = require('shCore').SyntaxHighlighter : null;
 
@@ -898,3 +684,15 @@ SyntaxHighlighter.brushes.Xml = function () {
 
 SyntaxHighlighter.brushes.Xml.prototype = new SyntaxHighlighter.Highlighter();
 SyntaxHighlighter.brushes.Xml.aliases   = ['xml', 'html', 'XML', 'HTML'];
+
+
+// For Other
+SyntaxHighlighter.brushes.Other = function () {
+    this.regexList = [
+        {regex:SyntaxHighlighter.regexLib.singleLinePerlComments,css:'comments'},
+        {regex:/^\-[^\-]{1}.*$/gm,css:'comments'}
+    ]
+};
+
+SyntaxHighlighter.brushes.Other.prototype = new SyntaxHighlighter.Highlighter();
+SyntaxHighlighter.brushes.Other.aliases   = ['other', 'Other'];
