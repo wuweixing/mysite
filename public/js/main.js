06f61610ab4f85a86d24f2fc019654092179431a
Ruchee <my@ruchee.com>
2013-10-11 15:11:06 +0800

    improved main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,25 +1,32 @@
 // 显示最新日志和最新文章
 $(function() {
-    $.get('/api_last_articles.php?action=get_journals', function(data) {
-        var html = make_html(data);
-        $('#journal').html(html);
-    });
+    var href = window.location.href;
 
-    $.get('/api_last_articles.php?action=get_articles', function(data) {
-        var html = make_html(data);
-        $('#article').html(html);
-    });
+    // 服务器上浏览才发送AJAX请求并显示最近文章
+    if (!/^file/.test(href)) {
+        $('#show_new').show();
 
-    // 生成HTML内容
-    function make_html (arr) {
-        data = eval(arr);
-        var html = '';
-        for (var i = 0; i < data.length; ++i) {
-            html += '<a href="'+data[i].path+'">'+data[i].title+'</a>';
-            if (i < data.length-1) {
-                html += '<hr />';
+        $.get('/api_last_articles.php?action=get_journals', function(data) {
+            var html = make_html(data);
+            $('#journal').html(html);
+        });
+
+        $.get('/api_last_articles.php?action=get_articles', function(data) {
+            var html = make_html(data);
+            $('#article').html(html);
+        });
+
+        // 生成HTML内容
+        function make_html (arr) {
+            data = eval(arr);
+            var html = '';
+            for (var i = 0; i < data.length; ++i) {
+                html += '<a href="'+data[i].path+'">'+data[i].title+'</a>';
+                if (i < data.length-1) {
+                    html += '<hr />';
+                }
             }
+            return html;
         }
-        return html;
     }
 });
