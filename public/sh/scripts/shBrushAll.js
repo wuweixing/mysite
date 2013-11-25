faef2bae8754b7f5543738a7595b1b4e74ac518c
Ruchee <my@ruchee.com>
2013-11-25 09:02:31 +0800

    updated for backup

--- public/sh/scripts/shBrushAll.js
+++ public/sh/scripts/shBrushAll.js
@@ -68,59 +68,6 @@ SyntaxHighlighter.brushes.Cpp.prototype = new SyntaxHighlighter.Highlighter();
 SyntaxHighlighter.brushes.Cpp.aliases   = ['c', 'cpp'];
 
 
-// For D
-(function() {
-    typeof(require) != 'undefined' ? SyntaxHighlighter = require('shCore').SyntaxHighlighter : null;
-
-    function Brush() {
-        var builtins  = 'toString toHash opCmp opEquals ' +
-                        'opUnary opBinary opApply opCall opAssign opIndexAssign opSliceAssign opOpAssign ' +
-                        'opIndex opSlice opDispatch ' +
-                        'toString toHash opCmp opEquals Monitor factory classinfo vtbl offset getHash ' +
-                        'equals compare tsize swap next init flags offTi destroy postblit toString toHash ' +
-                        'factory classinfo Throwable Exception Error capacity reserve assumeSafeAppend clear ' +
-                        'ModuleInfo ClassInfo MemberInfo TypeInfo';
-
-        var properties = '.sizeof .stringof .mangleof .nan .init .alignof .max .min .infinity .epsilon .mant_dig ' +
-                        '.max_10_exp .max_exp .min_10_exp .min_exp .min_normal .re .im';
-
-        var special_tokens = '__FILE__ __LINE__ __DATE__ __EOF__ __TIME__ __TIMESTAMP__ __VENDOR__ __VERSION__ #line';
-
-        var keywords =  '@property @disable abstract alias align asm assert auto body bool break byte case cast catch ' +
-                        'cdouble cent cfloat char class const continue creal dchar debug default delegate delete deprecated ' +
-                        'do double else enum export extern false final finally float for foreach foreach_reverse ' +
-                        'function goto idouble if ifloat immutable import in inout int interface invariant ireal ' +
-                        'is lazy long macro mixin module new nothrow null out override package pragma private ' +
-                        'protected public pure real ref return scope shared short static struct super switch ' +
-                        'synchronized template this throw true try typedef typeid typeof ubyte ucent uint ulong ' +
-                        'union unittest ushort version void volatile wchar while with __gshared ' +
-                        '__thread __traits ' +
-                        'string wstring dstring size_t hash_t ptrdiff_t equals_t ';
-
-        this.regexList = [
-            { regex: /\/\/\/.*$/gm, css: 'color3' },
-            { regex: SyntaxHighlighter.regexLib.singleLineCComments, css: 'comments' },
-            { regex: SyntaxHighlighter.regexLib.multiLineCComments, css: 'comments' },
-            { regex: /\/\+[\s\S]*?\+\//g, css: 'color1' },
-            { regex: SyntaxHighlighter.regexLib.multiLineDoubleQuotedString, css: 'string' },
-            { regex: SyntaxHighlighter.regexLib.multiLineSingleQuotedString, css: 'string' },
-            { regex: SyntaxHighlighter.regexLib.doubleQuotedString, css: 'string' },
-            { regex: SyntaxHighlighter.regexLib.singleQuotedString, css: 'string' },
-            { regex: new RegExp(this.getKeywords(properties), 'gm'), css: 'color2' },
-            { regex: new RegExp(this.getKeywords(special_tokens), 'gm'), css: 'constants' },
-            { regex: new RegExp(this.getKeywords(builtins), 'gm'), css: 'color2' },
-            { regex: new RegExp(this.getKeywords(keywords), 'gm'), css: 'keyword' }
-        ];
-    };
-
-    Brush.prototype = new SyntaxHighlighter.Highlighter();
-    Brush.aliases   = ['d'];
-
-    SyntaxHighlighter.brushes.D = Brush;
-    typeof(exports) != 'undefined' ? exports.Brush = Brush : null;
-})();
-
-
 // For PHP
 SyntaxHighlighter.brushes.Php = function() {
     var funcs = 'abs acos acosh addcslashes addslashes ' +
@@ -183,3 +130,60 @@ SyntaxHighlighter.brushes.Php = function() {
 
 SyntaxHighlighter.brushes.Php.prototype = new SyntaxHighlighter.Highlighter();
 SyntaxHighlighter.brushes.Php.aliases   = ['php'];
+
+
+// For Ruby
+SyntaxHighlighter.brushes.Ruby = function() {
+    var keywords = 'alias and BEGIN begin break case class def define_method defined do each else elsif ' +
+        'END end ensure false for if in module new next nil not or raise redo rescue retry return ' +
+        'self super then throw true undef unless until when while yield';
+
+    var builtins = 'Array Bignum Binding Class Continuation Dir Exception FalseClass File::Stat File Fixnum Fload ' +
+        'Hash Integer IO MatchData Method Module NilClass Numeric Object Proc Range Regexp String Struct::TMS Symbol ' +
+        'ThreadGroup Thread Time TrueClass';
+
+    this.regexList = [
+        { regex: SyntaxHighlighter.regexLib.singleLinePerlComments, css: 'comments' },
+        { regex: SyntaxHighlighter.regexLib.doubleQuotedString, css: 'string' },
+        { regex: SyntaxHighlighter.regexLib.singleQuotedString, css: 'string' },
+        { regex: /\b[A-Z0-9_]+\b/g, css: 'constants' },
+        { regex: /:[a-z][A-Za-z0-9_]*/g, css: 'color2' },
+        { regex: /(\$|@@|@)\w+/g, css: 'variable bold' },
+        { regex: new RegExp(this.getKeywords(keywords), 'gm'), css: 'keyword' },
+        { regex: new RegExp(this.getKeywords(builtins), 'gm'), css: 'color1' }
+    ];
+
+    this.forHtmlScript(SyntaxHighlighter.regexLib.aspScriptTags);
+};
+
+SyntaxHighlighter.brushes.Ruby.prototype = new SyntaxHighlighter.Highlighter();
+SyntaxHighlighter.brushes.Ruby.aliases   = ['ruby'];
+
+
+// For CoffeeScript
+SyntaxHighlighter.brushes.CoffeeScript = function() {
+    var csKeywords, jsKeywords, keywords;
+    jsKeywords = 'if else new return try catch finally throw break continue for in while delete instanceof typeof switch super extends class case default do function var void with const let debugger enum export import native __extends __hasProp';
+    csKeywords = 'then unless and or is isnt not of by where when until';
+    keywords   = jsKeywords + ' ' + csKeywords;
+
+    this.regexList = [
+        { regex: SyntaxHighlighter.regexLib.singleLinePerlComments, css: 'comments' },
+        { regex: /\#\#\#[\s\S]*?\#\#\#/gm, css: 'comments'},
+        { regex: SyntaxHighlighter.regexLib.multiLineDoubleQuotedString, css: 'string'},
+        { regex: SyntaxHighlighter.regexLib.doubleQuotedString, css: 'string'},
+        { regex: SyntaxHighlighter.regexLib.singleQuotedString, css: 'string'},
+        { regex: /\'\'\'[\s\S]*?\'\'\'/gm, css: 'string'},
+        { regex: /\/\/\/[\s\S]*?\/\/\//gm, css: 'string'},
+        { regex: /\b([\d]+(\.[\d]+)?|0x[a-f0-9]+)\b/gmi, css: 'value'},
+        { regex: /(@[\w._]*|this[\w._]*)/g, css: 'variable bold'},
+        { regex: /(([\w._]+)::([\w._]*))/g, css: 'variable bold'},
+        { regex: /([\w._]+)\s*(?=\=)/g, css: 'variable bold'},
+        { regex: /(-&gt;|->|=&gt;|=>|===|==|=|>|&gt;|<|&lt;|\.\.\.|&&|&amp;&amp;|\|\||\!\!|\!|\+\+|\+|--|-|\[|\]|\(|\)|\{|\})|\?|\/|\*|\%/g, css: 'keyword'},
+        { regex: new RegExp(this.getKeywords(keywords), 'gm'), css: 'keyword'}
+    ];
+    return;
+};
+
+SyntaxHighlighter.brushes.CoffeeScript.prototype = new SyntaxHighlighter.Highlighter();
+SyntaxHighlighter.brushes.CoffeeScript.aliases   = ['coffee'];
